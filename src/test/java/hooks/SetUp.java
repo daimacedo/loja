82184956e06f93ae6f756ceb37a29d02af184063
Daiane Macedo <daiane.macedo@hotmail.com>
2018-08-20 10:36:06 -0300

    gecko driver

--- src/test/java/hooks/SetUp.java
+++ src/test/java/hooks/SetUp.java
@@ -1,31 +1,23 @@
 package hooks;
 
-import java.io.File;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.concurrent.TimeUnit;
 
-import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 import constantes.URLs;
-import cucumber.api.Scenario;
-import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class SetUp extends URLs {
 
 	protected static WebDriver driver;
 
 	public SetUp() {
-		WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver();
+		System.setProperty("webdriver.gecko.driver","src/test/resources/drivers/geckodriver");
+		//WebDriverManager.chromedriver().setup();
+		driver = new FirefoxDriver();
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 
 	}
-
 
 }
--- src/test/java/stepdefinitions/RemoverItemDoCarrinhoTest.java
+++ src/test/java/stepdefinitions/RemoverItemDoCarrinhoTest.java
@@ -65,12 +65,12 @@ public class RemoverItemDoCarrinhoTest extends SetUp{
 	
 	@After
 	public void tearDown(Scenario cenario) throws IOException {
-		if (cenario.isFailed()) {
-			SimpleDateFormat formatoData = new SimpleDateFormat("yyyyMMddHH:mm:ss");
-			Calendar data = Calendar.getInstance();
-			File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-			FileUtils.copyFile(scrFile, new File("target/screenshots/" + cenario.getName().toString() + "_" + formatoData.format(data.getTime()).toString() + ".png"));
-		}
+//		if (cenario.isFailed()) {
+//			SimpleDateFormat formatoData = new SimpleDateFormat("yyyyMMddHH:mm:ss");
+//			Calendar data = Calendar.getInstance();
+//			File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+//			FileUtils.copyFile(scrFile, new File("target/screenshots/" + cenario.getName().toString() + "_" + formatoData.format(data.getTime()).toString() + ".png"));
+//		}
 		driver.quit();
 	}
 
